fe87f6ddd927de7874035f18b22f8b9c19b5ab93
malekbenhafed <serinebenhafed8@gmail.com>
2026-06-10 16:50:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🔩 Steel Plate Fault Detection App')
 
-st.write('Hello world!')
+st.write('A 🤖 machine learning web app that detects faults in steel plates')
